073751bcc52c14fe4edb0b54f3d32c701e64c6f5
Daniel Laughlin <danielclaughlin@gmail.com>
2026-02-23 19:20:08 +0000

    version 1.5

--- inst/doc/selectSpecies.R
+++ inst/doc/selectSpecies.R
@@ -1,4 +1,4 @@
-## ----setup, include = FALSE----------------------------------------------
+## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
@@ -7,7 +7,7 @@ library(Select)
 library(lattice)
 library(FD)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ### Set size of species pool
 Spp <- 5
 
@@ -18,33 +18,33 @@ trait <- as.matrix(data.frame(trait = c(1:Spp)))
 ### Most datasets have species names as row names, so let's add arbitrary row names using letters
 rownames(trait) <- c(letters[1:nrow(trait)])
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ### Store results from selectSpecies() into 'result1'
 result1 <- selectSpecies(t2c = trait, constraints = c(trait=3.5), t2d = trait, obj = "H")
 
 # Use the plotProbs() function to plot the abundance distribution
 plotProbs(result1, trait, xlab = "Species")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ### compare result1 with identical maxent output from FD package. Be sure to have the FD package installed.
 round(FD::maxent(constr = c(3.5), states = trait)$prob, 5)
 round(t(result1$prob), 5)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ### Store results from selectSpecies() into 'result2'. Note the only difference with result1 is a different objective function (Q).
 result2 <- selectSpecies(t2c = trait, constraints = c(trait=3.5), t2d = trait, obj = "Q")
 
 # plot result2
 plotProbs(result2, trait, xlab = "Species")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ### Store results from selectSpecies() into 'result3'. Note the only difference with result2 is a different objective function (QH)
 result3 <- selectSpecies(t2c = trait, constraints = c(trait=3.5), t2d = trait, obj="QH")
 
 # plot result3
 plotProbs(result3, trait, xlab = "Species")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ### Store results from selectSpecies() into 'result4'
 result4 <- selectSpecies(t2d = trait, obj = "QH")
 
@@ -52,14 +52,14 @@ result4 <- selectSpecies(t2d = trait, obj = "QH")
 plotProbs(result4, trait, xlab = "Species")
 
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ### 2-dimensional trait matrix of 16 species, evenly spaced between trait values 1 through 4
 trait.matrix <- as.matrix(cbind(traitX = c(rep(1,4), rep(2,4), rep(3,4), rep(4,4)), traitY = c(rep(c(1,2,3,4),4))))
 
 ### specify species names
 rownames(trait.matrix) <- c(letters[1:16])
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ### Create two evenly-spaced orthogonal traits
 traitX <- matrix(c(rep(1,4), rep(2,4), rep(3,4), rep(4,4)))
 traitY <- matrix(c(rep(c(1,2,3,4),4)))
@@ -73,7 +73,7 @@ result5 <- selectSpecies(t2c = traitX, constraints = c(traitX=3.5), t2d = traitY
 trait.matrix <- cbind(traitX, traitY)
 plotProbs(result5, trait.matrix, cex.lab = 0.7)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ### Store results from selectSpecies() into 'result6'
 # obj: select the QH objective function to maximize both Rao's Q and H, and cap the distance matrix by setting capd = TRUE
 result6 <- selectSpecies(t2c = traitX, constraints = c(traitX=3.5), t2d = traitY, obj = "QH", capd = TRUE)
@@ -81,9 +81,9 @@ result6 <- selectSpecies(t2c = traitX, constraints = c(traitX=3.5), t2d = traitY
 ### plot result6
 plotProbs(result6, trait.matrix, cex.lab = 0.7)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ### load serpentine data (available in the R package) 
-serpentine <- read.csv("traits.serpentine.california.csv", header = TRUE, row.names = 1)
+data(serpentine)
 
 ### take logs and rescale to unit variance
 wue <- data.frame(wue = scale(log(serpentine$wue)))
@@ -97,7 +97,7 @@ rownames(rootdepth) <- rownames(serpentine)
 wue.constraint <- c(quantile(wue$wue, 0.67))
 names(wue.constraint) <- c("wue")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ### Store results from selectSpecies() into 'result7'
 result7 <- selectSpecies(t2c = as.matrix(wue), constraints = c(wue.constraint), t2d = as.matrix(rootdepth), capd = TRUE, obj = "QH")
 
